1aecd5b0b8b71e72c259f12b76835042154ddbfa
shivam123425 <shivam123425@gmail.com>
2018-05-24 20:40:30 +0530

    Add passport and google-oauth

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({
-    hi: "there"
-  });
-});
+// client ID-  
 
-const PORT = process.env.PORT || 5000 
+// Client secret
+
+passport.use(new GoogleStrategy());
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server has started");
 });
