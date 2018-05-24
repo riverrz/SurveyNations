41a51b2125752ded40335152953f1a38871835f3
shivam123425 <shivam123425@gmail.com>
2018-05-24 18:58:59 +0530

    Corrected PORT env variable typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.port || 5000 
+const PORT = process.env.PORT || 5000 
 app.listen(PORT, () => {
   console.log("Server has started");
 });
