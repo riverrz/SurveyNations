17982e14beec69fce919a83b2b9c93d21cf1a458
shivam123425 <shivam123425@gmail.com>
2018-05-24 18:48:00 +0530

    Initial commit with scripts for deployment

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({
+    hi: "there"
+  });
+});
+
+const PORT = process.env.port || 5000 
+app.listen(PORT, () => {
+  console.log("Server has started");
+});
