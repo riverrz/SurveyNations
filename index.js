fb73bc2485290e31112863a1c217c194a4948a16
shivam123425 <shivam123425@gmail.com>
2018-06-02 01:03:01 +0530

    Add mongoose.connect to mlab survey-nations-dev

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {mongoURI} = require("./config/keys");
 require("./services/passport"); // Since passport.js doesnt return something no need to assign to a variable
 // this will be just executed directly.
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(mongoURI);
 
 authRoutes(app); // this passes app in the authRoutes thus app.get and app.post will work
 
