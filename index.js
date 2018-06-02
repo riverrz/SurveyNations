d1ec5f011c5fb6e4bdd9fabe5af209ccc08c33b7
shivam123425 <shivam123425@gmail.com>
2018-06-02 15:29:31 +0530

    Add user model and logic to add new user in mongodb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const {mongoURI} = require("./config/keys");
+require("./models/User");
 require("./services/passport"); // Since passport.js doesnt return something no need to assign to a variable
 // this will be just executed directly.
+
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,12 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users"); // this just pulls out the users collections
+// This allows us to not export model in User.js , as when User.js is run users will be loaded in mongoose
+// and can be pull by this statement, this is preferred method
+// MAKE SURE passport.js IS REQUIRED AFTER User.js IN index.js
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +16,14 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("Access Token: ", accessToken);
-      console.log("Refresh Token", refreshToken);
-      console.log("Profile: ", profile);
+      User.findOne({googleId: profile.id}).then((existingUser)=> {
+        if (existingUser) {
+          // already have this user in database
+        } else {
+          // make a new record for this user
+          new User({ googleId: profile.id }).save();
+        }
+      })
     }
   )
 ); // callbackURL is the URL which user will be redirected to after he grants permission
