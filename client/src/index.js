91ed3e6cf1d15eaee31573e561248bc31f66db71
shivam123425 <shivam123425@gmail.com>
2018-09-07 20:16:52 +0530

    Setup authReducer with default case

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-
+import rootReducer from "./reducers/index";
 import App from "./components/App";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(rootReducer, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
