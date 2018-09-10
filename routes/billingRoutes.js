41a1582270ac729bfdccb72feb4a24f8caccbfed
shivam123425 <shivam123425@gmail.com>
2018-09-10 19:02:32 +0530

    Add credits on User, add credits on successful charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -10,6 +10,8 @@ module.exports = app => {
       description: "$5 for 5 credits",
       source: req.body.id
     });
-    console.log(charge);
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };
