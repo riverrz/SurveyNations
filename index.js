e4587ef85c376a5dec4acaba92414ea2162b4945
shivam123425 <shivam123425@gmail.com>
2018-09-10 23:29:51 +0530

    Server static assets in production

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ mongoose.connect(mongoURI);
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log("Server has started");
 });
