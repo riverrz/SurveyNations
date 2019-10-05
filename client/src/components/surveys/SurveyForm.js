d7df6f5135fdbf771dee55d5335b50af19b70383
shivam123425 <shivam123425@gmail.com>
2019-10-05 15:19:49 +0530

    Change email to recipients

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -41,7 +41,7 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
-  errors.emails = validateEmails(values.emails || "");
+  errors.recipients = validateEmails(values.recipients || "");
   _.each(formFields, ({ name }) => {
     if (!values[name]) {
       errors[name] = "You must provide a value";
