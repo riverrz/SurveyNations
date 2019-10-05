517a285b26a59cc7ce5d1cb709afe005ef751dee
shivam123425 <shivam123425@gmail.com>
2019-10-05 15:28:53 +0530

    Send POST request /api/surveys when survey is submitted and reflect the deduction

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async dispatch => {
-  const response = await axios.get("/api/current_user");
-  dispatch({ type: FETCH_USER, payload: response.data });
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = token => async dispatch => {
@@ -12,6 +12,8 @@ export const handleToken = token => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = values => {
-  return { type: 'submit_survey' }
-}
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post("/api/surveys", values);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
