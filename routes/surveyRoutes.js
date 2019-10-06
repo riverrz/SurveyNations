db0ce34717e5fa77f612bbe1d5259310e80f48fe
shivam123425 <shivam123425@gmail.com>
2019-10-06 15:24:25 +0530

    Change /api/surveys/thanks to /api/surveys/:surveyId/:choice and set lastResponded when saving Survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for your feedback");
   });
   app.post("/api/surveys/webhooks", (req, res) => {
@@ -37,12 +37,12 @@ module.exports = app => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { "recipients.$.responded": true }
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
           }
         ).exec();
       })
       .value();
-    console.log("Received");
     res.send({});
   });
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
