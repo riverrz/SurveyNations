1b0728db285996eff35c9e4beb72cf433e271b1b
shivam123425 <shivam123425@gmail.com>
2019-10-06 16:49:32 +0530

    Display latest surveys on top

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,7 +7,7 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
   renderSurveys() {
-    return this.props.surveys.map(survey => {
+    return this.props.surveys.reverse().map(survey => {
       return (
         <div className="card blue-grey darken-1" key={survey._id}>
           <div className="card-content white-text">
