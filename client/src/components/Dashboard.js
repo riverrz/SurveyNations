66b2237921394d995e96538b4ed699b3b02af4f8
shivam123425 <shivam123425@gmail.com>
2019-10-06 16:46:53 +0530

    Render surveys on the dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SurveyList from "./surveys/SurveyList";
 
 const Dashboard = props => {
   return (
     <div>
       Dashboard
+      <SurveyList />
       <div className="fixed-action-btn">
         <Link className="btn-floating btn-large red" to="/surveys/new">
           <i className="material-icons">add</i>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
-import authReducer from "./authReducer";
 import { reducer as reduxForm } from "redux-form";
+import authReducer from "./authReducer";
+import surveysReduer from "./surveysReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReduer
 });
